6dfe167a28b507d990d406b370bd019b129b508a
Liza Svitanko <eisvitanko@edu.hse.ru>
2020-03-29 16:34:50 +0300

    Вёрстка (#1)
    * hw1 partly done
    
    * almost done
    
    * Update README.md
    
    * bug fix
    
    * исправлены комментарии, изменена структура проекта
    
    * Update README.md
    
    * добавила отображение адаптивной верстки
    
    * поправила структуру проекта
    
    * small fix

---
+++ src/start/start.js
@@ -0,0 +1,7 @@
+require('normalize.css/normalize.css');
+require('../css/blocks/header.css');
+require('../css/blocks/global.css');
+require('../css/blocks/footer.css');
+require('./start.css');
+
+require('../images/instruments.svg');
